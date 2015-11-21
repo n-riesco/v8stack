989f0c02401ed99d43b6c033043b43bfa9b94533
Nicolas Riesco <enquiries@nicolasriesco.net>
2015-11-21 08:33:30 +0000

    tests: Move asserts after `v8stack.disable()`

--- test.js
+++ test.js
@@ -3,17 +3,20 @@ var v8stack = require("./index.js");
 var assert = require("assert");
 
 var error;
+var stack;
 var prepareStackTrace = Error.prepareStackTrace;
 var stackTraceLimit = 5;
 
 v8stack.enable();
 Error.stackTraceLimit = stackTraceLimit;
 error = new Error();
+stack = v8stack.evaluate(error);
+v8stack.disable();
+
 assert.strictEqual(
     v8stack.evaluate(error).length, stackTraceLimit,
     "Error.stackTraceLimit not honoured"
 );
-v8stack.disable();
 assert.deepEqual(
     Error.prepareStackTrace, prepareStackTrace,
     "Error.prepareStackTrace not restored"
@@ -25,6 +28,13 @@ prepareStackTrace = function() {
 };
 Error.prepareStackTrace = prepareStackTrace;
 error = new Error();
+stack = v8stack.evaluate(error);
+v8stack.disable();
+
+assert.deepEqual(
+    Error.prepareStackTrace, prepareStackTrace,
+    "Error.prepareStackTrace honoured but not restored"
+);
 assert.strictEqual(
     v8stack.evaluate(error).length, stackTraceLimit,
     "Error.stackTraceLimit not honoured"
@@ -32,9 +42,4 @@ assert.strictEqual(
 assert.strictEqual(
     error.stack, "test",
     "Error.prepareStackTrace not honoured"
-);
-v8stack.disable();
-assert.deepEqual(
-    Error.prepareStackTrace, prepareStackTrace,
-    "Error.prepareStackTrace honoured but not restored"
 );
